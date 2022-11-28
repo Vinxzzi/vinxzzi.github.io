59419ed23f0cce7f3b0715090f1921db7d98652f
Vinxzzi <vinxz0014@gmail.com>
2022-11-28 16:11:26 -0300

    atualizacao

---
+++ sacola-api/build/resources/main/data.sql
@@ -0,0 +1,15 @@
+INSERT INTO restaurante(id, cep, complemento, nome) VALUES
+(1L, '0000001', 'Complemento Endereco Restaurante 1', 'Restaurante1'),
+(2L, '0000002', 'Complemento Endereco Restaurante 2', 'Restaurante2');
+
+INSERT INTO cliente(id, cep, complemento, nome)VALUES
+(1L, '0000001', 'Complemento Endereco Restaurante 1', 'Cliente 1');
+
+
+INSERT INTO produto(id, disponivel, nome, valor_unitario,restaurante_id)VALUES
+(1L, true, 'Produto 1', 5.0, 1L),
+(2L, true, 'Produto 2', 6.0, 1L),
+(3L, true, 'Produto 3', 7.0, 1L);
+
+INSERT INTO sacola(id, forma_pagamento, fechada, valor_total, cliente_id) VALUES
+(1L, 0, false, 0.0, 1L);
